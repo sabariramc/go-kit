4bd9f0cfa7befe4569e69d2ed62e4df0c5affd7f
Sabariram <c.sabariram@gmail.com>
2025-07-10 12:44:51 +0530

    added json module

---
+++ json/json.go
@@ -0,0 +1,31 @@
+package correlation
+
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+// CustomJSONTagHandler is a wrapper around jsoniter.API to handle JSON encoding/decoding
+// with a custom struct tag.
+type CustomJSONTagHandler struct {
+	jsoniter.API
+}
+
+// NewCustomJSONTagHandler creates a new CustomJSONTagHandler with a custom struct tag.
+// It sets up the JSON configuration to escape HTML, sort map keys, validate JSON raw messages,
+// and use the specified tag key.
+func NewCustomJSONTagHandler(tag string) *CustomJSONTagHandler {
+	return &CustomJSONTagHandler{
+		API: jsoniter.Config{
+			EscapeHTML:             true,
+			SortMapKeys:            true,
+			ValidateJsonRawMessage: true,
+			TagKey:                 tag,
+		}.Froze(),
+	}
+}
+
+// HeaderJSON is a CustomJSONTagHandler instance with the custom tag "header".
+var HeaderJSON = NewCustomJSONTagHandler("header")
+
+// BodyJSON is a CustomJSONTagHandler instance with the custom tag "body".
+var BodyJSON = NewCustomJSONTagHandler("body")
