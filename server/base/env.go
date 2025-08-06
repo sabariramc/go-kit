d5c709295244c01ec8f4677c4c00454321b3c3a9
Sabariram <c.sabariram@gmail.com>
2025-08-06 14:27:30 +0530

    Added get service name function

--- server/base/env.go
+++ server/base/env.go
@@ -1,5 +1,17 @@
 package base
 
+import (
+	"sync"
+
+	"github.com/sabariramc/go-kit/env"
+)
+
 const (
 	EnvServiceName = "SERVICE_NAME"
+)
+
+var GetServiceName = sync.OnceValue(
+	func() string {
+		return env.Get(EnvServiceName, "default-service")
+	},
 )
